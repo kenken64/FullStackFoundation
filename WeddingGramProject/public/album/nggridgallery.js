ee1bba5dacb63cf0043b3180bd5b584ae8556529
qjawe <qqjawe@gmail.com>
2017-02-16 19:35:32 +0800

    BackEnd - Shell Template FUCK!!

--- WeddingGramProject/public/album/nggridgallery.js
+++ WeddingGramProject/public/album/nggridgallery.js
@@ -1,4 +1,4 @@
-angular.module('demoApp', ['angularGrid', 'ngMaterial'])
+angular.module('demoApp', ['angularGrid'])
     .service('imageService',['$q','$http',function($q,$http){
         this.loadImages = function(){
             return $http.jsonp("https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK");
