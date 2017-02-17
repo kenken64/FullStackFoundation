1abe78cd8fc2fc91ac451f1b404938a45ce0a603
qjawe <qqjawe@gmail.com>
2017-02-17 17:11:25 +0800

    ng-app="RegfApp"

--- Day-10W1/AssessmentDay-userregform/public/vfapp.js
+++ Day-10W1/AssessmentDay-userregform/public/vfapp.js
@@ -1,16 +1,1 @@
-// vfapp.js
-// create angular app
-var validformApp = angular.module('validformApp', []);
-
-// create angular controller
-validformApp.controller('mainController', function($scope) {
-
-  // function to submit the form after all validation has occurred
-  $scope.submitForm = function(isValid) {
-
-    // check to make sure the form is completely valid
-    if (isValid) {
-      alert('the form is amazing & working fine');
-    }
-  };
-});
+var vfapp = angular.module('vfapp', ['ngMessages']);
