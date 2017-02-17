c3efd4b167147093a0dc52eb0ef30dc6d3ca95ac
qjawe <qqjawe@gmail.com>
2017-02-17 17:39:53 +0800

    VFormApp

--- Day-10W1/AssessmentDay-userregform/public/app.js
+++ Day-10W1/AssessmentDay-userregform/public/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('app', ['ngMessages']);
+(function() {
+	var VFormApp = angular.module("VFormApp", ["ServiceVFormApp"]);
+
+	var vformctrl = function(FormsSvc) {
+		var vformctrl = this;
+
+
+		vformctrl.getMandatory = function() {
+			FormsSvc.getMandatory()
+				.then(function(resultsMandatory) {
+					console.log(resultsMandatory);
+				});
+
+
+		};
+
+		//Load the first order
+		//vformctrl.getMandatory();
+	};
+	vformctrl.$inject = [ "FormsSvc" ];
+
+	VFormApp.controller("vformctrl", vformctrl);
+})();
