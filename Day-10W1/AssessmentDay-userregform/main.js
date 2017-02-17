ac9134d66b81f40eacf0ec013b4419a4456eb8d0
qjawe <qqjawe@gmail.com>
2017-02-17 10:48:59 +0800

    bower set path

--- Day-10W1/AssessmentDay-userregform/main.js
+++ Day-10W1/AssessmentDay-userregform/main.js
@@ -6,9 +6,13 @@ var express = require ("express");
 // This create an instance of express app
 var app = express();
 
-// set the directory of the publick folder
+// set the directory of the public folder
 app.use(express.static(__dirname + "/public") );
-app.use(express.static(__dirname + "/bower_components") );
+// set the directory of the bower name = folder at /bower_components
+// this is set another name or alias to the target path
+// In HTML should set /bower/angular.js == /bower_components/angular.js
+app.use("/bower", express.static(__dirname + "/bower_components") );
+
 
 //Configure the port
 app.set("port", process.env.APP_PORR | 3000);
