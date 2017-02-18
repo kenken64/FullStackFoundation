f34c2a5c2bb16588b1fb5268fedac9aa19344828
qjawe <qqjawe@gmail.com>
2017-02-18 14:23:46 +0800

    API Call back
    https://api.flickr.com/services/feeds/photos_public.gne?format=json&json
    callback=JSON_CALLBACK

--- Day-10W1/AssessmentDay-userregform/public/vfapp.js
+++ Day-10W1/AssessmentDay-userregform/public/vfapp.js
@@ -1,1 +1,52 @@
-var vfapp = angular.module('vfapp', ['ngMessages']);
+(function() {
+	var ServiceVFormApp = angular.module("ServiceVFormApp", []);
+
+
+	var ServiceVFormAppSvc = function($http) {
+		var ServiceVFormAppSvc = this;
+
+		var getPhotos = function(base, symbol) {
+			var myparams = {};
+
+			if (base)
+				myparams.base = base.toUpperCase();
+
+			if (symbols)
+				myparams.symbols = symbols.toUpperCase();
+
+			// GET /latest photos
+			var promise = $http.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK", { 
+					params: myparams
+				}
+			);
+
+			return (promise);
+		}
+
+		ServiceVFormAppSvc.getMandatory = function(base, symbols, fn) {
+
+			var promise = getPhotos(base, symbols);
+
+			promise.then(function(result) {
+				fn(result.data);
+			});
+		};
+
+		ServiceVFormAppSvc.getMan = function(base, symbols) {
+
+			var promise = getPhotos(base, symbols);
+
+			var promise2 = promise.then(function(result) {
+				var ratesOnly = result.data;
+				return (ratesOnly);
+			});
+
+			return (promise2);
+		}
+	}
+
+	//ServiceVFormApp.$inject = [ "$http" ];
+	//ServiceVFormApp.service("ServiceVFormAppSvc", ServiceVFormAppSvc);
+
+	ServiceVFormApp.service("ServiceVFormAppSvc", ["$http", ServiceVFormAppSvc]);
+})();
