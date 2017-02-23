50eee83b17d065de3e14ea3444be0de149013ced
qjawe <qqjawe@gmail.com>
2017-02-23 10:54:53 +0800

    App updates

--- Day-13W2/sequelizejsconfig/server/app.js
+++ Day-13W2/sequelizejsconfig/server/app.js
@@ -7,7 +7,33 @@ const NODE_PORT = process.env.NODE_PORT || 8000;
 
 // TODO check the dirname is correct when test
 const CLIENT_FOLDER = path.join(__dirname + '/../client');
-
 const MSG_FOLDER = path.join(CLIENT_FOLDER + '/assets/messages');
 
+const MYSQL_USERNAME ="root";
+const MYSQL_PASSWORD ="hplanet";
+
 var app = express();
+
+var connection = new Sequelize(
+  "employees",
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  {
+      host: "localhost"
+      logging: console.log,
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min, 0,
+        idle: 10000,
+      }
+  }
+);
+
+var Department = require ("./models/department")(connection, Sequelize);
+var Employee = require ("./models/employee")(conn, Sequelize);
+var Manager = require ("./models/deptmanager")(conn, Squelize);
+
+
+app.use(express.static(CLIENT_FOLDER));
+app.use(bodyParser.json());
