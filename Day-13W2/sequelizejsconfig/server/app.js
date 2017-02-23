28a65ed367d19500967fbf942cdbba4ca23755a0
qjawe <qqjawe@gmail.com>
2017-02-23 15:55:02 +0800

    Post done

--- Day-13W2/sequelizejsconfig/server/app.js
+++ Day-13W2/sequelizejsconfig/server/app.js
@@ -44,8 +44,9 @@ app.use(express.static(CLIENT_FOLDER));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-// route path - retrieve all departments
+// route path - retrieve all departments - GET cmd API
 app.get("/api/departments", function(req, res){
+  console.log("in GET /api/departments");
     Department
         .findAll({
 
@@ -60,6 +61,35 @@ app.get("/api/departments", function(req, res){
         })
 });
 
+// route POST - retrieve all departments - POST cmd API
+app.post("/api/departments", function(req, res){
+  console.log(req.body);
+  console.log("in POST /api/departments");
+    res.status(200).json(req.body)
+
+});
+
+
+app.put(API_DEPARTMENTS_ENDPOINT + "/:dept_no", function(req, res) {
+    var where = {};
+    where.dept_no = req.param.dept_no;
+    Department
+      .updates({dept_name: req.body.dept_name}),
+      {where: where}
+    ).then(function (result) {
+        res
+            .status(200)
+            .json(result);
+      }).catch(function (err) {
+          console.log(err);
+          res
+              .status(500)
+              .json(err);
+      })
+})
+
+
+// listen server port number
 app.listen(NODE_PORT, function(){
     console.log("Server is running at port " + NODE_PORT);
 })
